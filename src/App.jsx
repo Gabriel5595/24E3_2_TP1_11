86ff75922a059b0055694dd0dc8515b40e8b074b
Gabriel Carvalho <110797653+Gabriel5595@users.noreply.github.com>
2024-08-12 11:22:03 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Saudacao from './components/Saudacao'
+
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div>
+      <Saudacao />
+    </div>
+  )
+}
---
+++ src/components/Saudacao.jsx
@@ -0,0 +1,7 @@
+export default function Saudacao() {
+    return (
+        <div>
+            <h1>Olá, Mundo!</h1>
+        </div>
+    )
+}
